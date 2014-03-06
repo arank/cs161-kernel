6c8cab7f8ec379cbc3d21e23589e65252fe4d926
ARAN KHANNA <arankhanna@college.harvard.edu>
2014-03-05 23:47:36 -0500

    test

--- kern/syscall/runprogram.c
+++ kern/syscall/runprogram.c
@@ -170,7 +170,7 @@ int sys_execv(const_userptr_t program, const_userptr_t *args){
 
 	}
 
-	lock_release(exec_lock);sdasd
+	lock_release(exec_lock);
 
 	// TODO is this the right addr for argv???
 	enter_new_process(argc, (userptr_t)stackptr+offset /*userspace addr of argv*/,
