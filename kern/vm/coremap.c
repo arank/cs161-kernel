b940d5c337685199d510f380f6e02a1622ffb0f4
ARAN KHANNA <arankhanna@college.harvard.edu>
2014-03-30 15:31:42 -0400

    added locking and unlocking for coremap

--- kern/vm/coremap.c
+++ kern/vm/coremap.c
@@ -19,3 +19,27 @@ struct coremap {
     const unsigned size;
     const struct *cme;
 };
+
+int core_set_in_use(int index){
+	spinlock_acquire(coremap->lock);
+	if(coremap->cme[index]->busybit == 0){
+		coremap->cme[index]->busybit = 1;
+		spinlock_release(coremap->lock);
+	}else{
+		spinlock_release(coremap->lock);
+		return 1;
+	}
+}
+
+
+int core_set_free(int index){
+	spinlock_acquire(coremap->lock);
+	if(coremap->cme[index]->busybit == 1){
+		coremap->cme[index]->busybit = 0;
+		spinlock_release(coremap->lock);
+		return 0;
+	}else{
+		spinlock_release(coremap->lock);
+		return 1;
+	}
+}
