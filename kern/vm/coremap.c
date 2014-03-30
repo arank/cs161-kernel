8611852540a13f07385dccde0ed5063a715bd3ad
ARAN KHANNA <arankhanna@college.harvard.edu>
2014-03-30 17:04:33 -0400

    Added get_free_cme

--- kern/vm/coremap.c
+++ kern/vm/coremap.c
@@ -12,14 +12,16 @@ struct cme {
 };
 
 struct coremap {
-    struct lock *lock;
-    struct cv *cv;
+    struct spinlock *lock;
     unsigned free;
     unsigned modified;
     const unsigned size;
     const struct *cme;
+    int last_allocated;
 };
 
+// We don't give the option to retry as that would
+// involve sleeping which could lead to livelock
 int core_set_in_use(int index){
 	spinlock_acquire(coremap->lock);
 	if(coremap->cme[index]->busybit == 0){
@@ -43,3 +45,36 @@ int core_set_free(int index){
 		return 1;
 	}
 }
+
+paddr_t get_free_cme(vaddr_t vpn, bool kern){
+
+	spinlock_aquire(coremap->lock);
+	int index = coremap->last_allocated;
+	spinlock_release(coremap->lock);
+
+	for(int i = 0; i < coremap->size; i++){
+		int index = (index+1) % coremap->size;
+		if(core_set_in_use(index) == 0){
+			// Check if in use
+			if(coremap->cme[index]->use == 0){
+				coremap->cme[index]->use = 1;
+				coremap->cme[index]->vpn = vpn;
+				coremap->cme[index]->pid = curproc->pid;
+				if(kern){
+					coremap->cme[index]->kern = 1;
+				}else{
+					coremap->cme[index]->kern = 0;
+				}
+				core_set_free(index);
+				// TODO possibly zero page here.
+				// Multiply by page size to get paddr
+				return index*4096;
+			}
+			// TODO add eviction later
+			core_set_free(index);
+		}
+	}
+}
+
+
+
