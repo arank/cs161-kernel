1bd3e0d260bb1d9999026bc0a6404e0ec7158e61
Ivan Sinyagin <isinyagin@fas.harvard.edu>
2014-04-03 00:19:27 -0400

    contigiuos alloc/free works

--- kern/arch/mips/include/vm.h
+++ kern/arch/mips/include/vm.h
@@ -68,6 +68,9 @@
 #define PADDR_TO_KVADDR(paddr) ((paddr)+MIPS_KSEG0)
 #define KVADDR_TO_PADDR(vaddr) ((vaddr)-MIPS_KSEG0)
 
+#define PADDR_TO_CMI(paddr) (paddr / PAGE_SIZE)
+#define CMI_TO_PADDR(i) (i * PAGE_SIZE)
+
 /*
  * The top of user space. (Actually, the address immediately above the
  * last valid user address.)
