0bf9ab2ff9b733976d6691d2936085ccdfd90453
Kenny Yu <yu.kenny124@gmail.com>
2014-04-15 01:44:14 -0400

    Zero cpu->c_cpinlocks on cpu_create. See piazza post @186.
    https://piazza.com/class/hp8nu5lcko4426?cid=186

--- kern/thread/thread.c
+++ kern/thread/thread.c
@@ -190,6 +190,7 @@ cpu_create(unsigned hardware_number)
 	c->c_curthread = NULL;
 	threadlist_init(&c->c_zombies);
 	c->c_hardclocks = 0;
+	c->c_spinlocks = 0;
 
 	c->c_isidle = false;
 	threadlist_init(&c->c_runqueue);
